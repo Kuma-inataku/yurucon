7d48c58dc229e9010f2ac71bb6a64c78a7dde1f5
takuya-inaguma <inataku0121@gmail.com>
2024-05-16 22:03:26 +0900

    phpcsfixer設定を最低限に変更

--- www/.php-cs-fixer.dist.php
+++ www/.php-cs-fixer.dist.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
-$finder = PhpCsFixer\Finder::create()
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+// @see: https://cs.symfony.com/doc/config.html#the-simplest-config
+$finder = Finder::create()
     ->in([
         __DIR__ . '/app',
         __DIR__ . '/config',
@@ -12,48 +16,10 @@ $finder = PhpCsFixer\Finder::create()
         __DIR__ . '/tests',
     ]);
 
-$config = new PhpCsFixer\Config();
-
-return $config
-    ->setRiskyAllowed(true)
+// @see: https://cs.symfony.com/doc/config.html#the-simplest-config
+return (new Config())
     ->setRules([
-        '@PhpCsFixer:risky' => true,
-        'blank_line_after_opening_tag' => true,
-        'linebreak_after_opening_tag' => true,
-        'declare_strict_types' => true,
-        'no_superfluous_phpdoc_tags' => false,
-        'global_namespace_import' => [
-            'import_classes' => true,
-            'import_constants' => true,
-            'import_functions' => true,
-        ],
-        'ordered_imports' => [
-            'sort_algorithm' => 'alpha',
-            'imports_order' => [
-                'class',
-                'function',
-                'const',
-            ],
-        ],
-        'no_unused_imports' => true,
-        'phpdoc_types_order' => [
-            'null_adjustment' => 'always_last',
-            'sort_algorithm' => 'none',
-        ],
-        'php_unit_test_case_static_method_calls' => [
-            'call_type' => 'this'
-        ],
-        'phpdoc_align' => [
-            'align' => 'left',
-        ],
-        'not_operator_with_successor_space' => true,
-        'blank_line_after_namespace' => true,
-        'final_class' => true,
-        'date_time_immutable' => true,
-        'declare_parentheses' => true,
-        'final_public_method_for_abstract_class' => true,
-        'mb_str_functions' => true,
-        'simplified_if_return' => true,
-        'simplified_null_return' => true,
+        '@PER-CS' => true,
+        '@PHP82Migration' => true,
     ])
     ->setFinder($finder);
