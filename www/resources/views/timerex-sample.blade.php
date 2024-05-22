ac36443de1f78c90d7f8bd9aaeea2994a09f49db
takuya-inaguma <inataku0121@gmail.com>
2024-05-22 13:08:55 +0900

    検証するAPI変更

--- www/resources/views/timerex-sample.blade.php
+++ www/resources/views/timerex-sample.blade.php
@@ -27,7 +27,7 @@
                 <div class="mt-16">
                         <a href="https://laravel-news.com" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
-                                <h2 class="text-xl font-semibold text-gray-900 dark:text-white">TimeRexアカウント作成(OAuth認証)</h2>
+                                <h2 class="text-xl font-semibold text-gray-900 dark:text-white">ZoomAPI検証</h2>
                             </div>
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
