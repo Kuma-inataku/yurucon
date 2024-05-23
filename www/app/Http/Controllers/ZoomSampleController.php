0a0c882872f4efba1a885e2c07eadd8770a5b9e4
takuya-inaguma <inataku0121@gmail.com>
2024-05-23 13:50:05 +0900

    create controller for zoomAPI

---
+++ www/app/Http/Controllers/ZoomSampleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ZoomSampleController extends Controller
+{
+    public function create() {
+        // todo: create a new meeting
+        
+    }
+
+    public function update() {
+        // todo: update a new meeting
+
+    }
+
+    public function delete() {
+        // todo: delete a new meeting
+
+    }
+}
